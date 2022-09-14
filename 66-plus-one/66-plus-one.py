99e71c968ec7035dcbb9d858de11423ec5c933b1
Esocle <91296140+Esocle@users.noreply.github.com>
2022-09-14 16:39:13 +0900

    Time: 29 ms (98.05%), Space: 14 MB (12.21%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,9 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        temp = ''
-        for e in digits:
-            e = str(e)
-            temp += e
+        temp = ''.join(map(str, digits))
         temp = int(temp)
         temp += 1
         n_list = list(map(int, str(temp)))
