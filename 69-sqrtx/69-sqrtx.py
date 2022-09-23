1435db8e412d60735e988c07dc00c52fc250562d
Esocle <91296140+Esocle@users.noreply.github.com>
2022-09-23 15:32:48 +0900

    Time: 78 ms (36.36%), Space: 13.8 MB (56.34%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,6 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        div = 0
-        while ((div + 1) * (div + 1) <= x):
-            div += 1
-        return div
+        left = 0
+        right = x
+        
+        while left <= right :
+            mid = (left + right) // 2 
+            calc = mid * mid
+            if calc == x :
+                return mid
+            elif calc < x :
+                left = mid + 1
+            else :
+                right = mid - 1
+        else :
+            return right
