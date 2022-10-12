d445dd0f7fbcb8bf115ec46bc366fd9c60229d2d
Esocle <91296140+Esocle@users.noreply.github.com>
2022-10-12 12:31:22 +0900

    Delete 0001-two-sum directory

--- 0001-two-sum/0001-two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            temp = target - nums[i]
-            try:
-                if i != nums.index(temp):
-                    return [i, nums.index(temp)]                  
-            except:
-                continue
