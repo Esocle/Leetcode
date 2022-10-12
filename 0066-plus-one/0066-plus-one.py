f28727adbe0e3f8d342b273b4b6b56eb3cc675e3
Esocle <91296140+Esocle@users.noreply.github.com>
2022-10-12 12:32:21 +0900

    Delete 0066-plus-one directory

--- 0066-plus-one/0066-plus-one.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        temp = ''.join(map(str, digits))
-        temp = int(temp)
-        temp += 1
-        n_list = list(map(int, str(temp)))
-        
-        return n_list
